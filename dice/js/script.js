c4fa084f46663562057f2747ed42c01a42d7652d
Othman Taik <taikothman1997@gmail.com>
2020-10-22 18:01:44 +0200

    Dice Game 90%

--- dice/js/script.js
+++ dice/js/script.js
@@ -1,9 +1,24 @@
 var utente; //variabile aleatoria utente
 var pc; //variabile aleatoria pc
 playGame = document.getElementById("play")
+var utente = ""
+var pc = ""
 
-utente = Math.floor((Math.random()*6));
-pc = Math.floor((Math.random()*6));
+//Evento generatore del gioco
+//playGame.addEventListener("click",function(){
+
+  utente = Math.floor((Math.random()*6)+1);
+  pc = Math.floor((Math.random()*6)+1);
+
+  //Esito del gioco
+  if (utente > pc){
+    message = "YOU WIN!"
+  } else if (utente < pc) {
+    message = "YOU LOSE!"
+  } else if (utente = pc) {
+    message = "DROW!"
+  }
+
 
 
 
