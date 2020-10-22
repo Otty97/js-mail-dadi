99712cd1c27901502e47e9d2d8e8c6f5da2b0ec7
Othman Taik <taikothman1997@gmail.com>
2020-10-22 17:02:16 +0200

    dice game 10%

--- dice/js/script.js
+++ dice/js/script.js
@@ -1,1 +1,27 @@
-var utenti = ["tizio@gmail.com", "caio@gmail.com", "sempronio@gmail.com"]
+var utente; //variabile aleatorio dell'utente
+var pc; //variabile aleatorio dell'pc
+
+utente = Math.floor((Math.random()*6)+1);
+pc = Math.floor((Math.random()*6)+1);
+
+if (utente > pc) {
+
+} else if (utente < pc) {
+
+} else if (utente = pc) {
+
+}
+
+document.getElementById("finalResult").innerHTML = message;
+
+
+//var utenti = ["tizio@gmail.com", "caio@gmail.com", "sempronio@gmail.com"]
+
+//var inputUtenti = prompt("inserisci email")
+
+//var message = "Email non registrata"
+//for (var i=0; i < utenti.length; i++){
+//  if(utenti[i] == inputUtenti){
+//    message = "Email verificata"
+//  }
+//}
