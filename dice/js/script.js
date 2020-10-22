bedaf8d070befceb92f30430b19874d1f6a40164
Othman Taik <taikothman1997@gmail.com>
2020-10-22 17:34:59 +0200

    Dice Game js 40%

--- dice/js/script.js
+++ dice/js/script.js
@@ -1,32 +1,14 @@
-var utente; //variabile aleatorio dell'utente
-var pc; //variabile aleatorio dell'pc
+var utente; //variabile aleatoria utente
+var pc; //variabile aleatoria pc
 playGame = document.getElementById("play")
 
-utente = Math.floor((Math.random()*6)+1);
-pc = Math.floor((Math.random()*6)+1);
-
-//Evento generatore del gioco
-playGame.addEventListener("click",function(){
+utente = Math.floor((Math.random()*6));
+pc = Math.floor((Math.random()*6));
 
 
-if (utente > pc) {
 
-} else if (utente < pc) {
 
-} else if (utente = pc) {
 
-}
-
+document.getElementById("utenteResult").innerHTML = utente;
+document.getElementById("pcResult").innerHTML = pc;
 document.getElementById("finalResult").innerHTML = message;
-
-
-//var utenti = ["tizio@gmail.com", "caio@gmail.com", "sempronio@gmail.com"]
-
-//var inputUtenti = prompt("inserisci email")
-
-//var message = "Email non registrata"
-//for (var i=0; i < utenti.length; i++){
-//  if(utenti[i] == inputUtenti){
-//    message = "Email verificata"
-//  }
-//}
